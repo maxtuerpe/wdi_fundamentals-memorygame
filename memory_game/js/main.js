3afa685d982bffa4a0d741378ee9c6cd3324a997
Max Tuerpe <maxtuerpe4@gmail.com>
2018-08-31 15:28:58 -0600

    more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,18 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped" + " " +cardOne)
+console.log ("up and running");
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [cardOne, cardTwo];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+console.log("User flipped" + " " +cardOne);
+console.log("User flipped" + " " +cardTwo);
+
+
+if (cardsInPlay.length === 2) {
+
+if (cardOne === cardTwo)
+	alert ("You found a match!")
+else  
+	alert("Sorry, try again.")
+}
