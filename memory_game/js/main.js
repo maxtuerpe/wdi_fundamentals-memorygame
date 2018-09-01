35159dbc06f8732ea4aa271aa1c854f3e805a5d1
Max Tuerpe <maxtuerpe4@gmail.com>
2018-08-31 19:29:15 -0600

    flippedCard logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,21 @@
 console.log ("up and running");
 
+
 var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [cardOne, cardTwo];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-console.log("User flipped" + " " +cardOne);
-console.log("User flipped" + " " +cardTwo);
-
-
-if (cardsInPlay.length === 2) {
-
-if (cardOne === cardTwo)
-	alert ("You found a match!")
-else  
-	alert("Sorry, try again.")
+var cardsInPlay = [];
+var chechForMatch = function(){
+	if (cardsInPlay.length === 2){
+		if (cardsInPlay[0] === cardsInPlay[1]){
+			alert ("You found a match!");
+		}else { 
+			alert("Sorry, try again.");
+		}
+	} 	
 }
+var flipCard = function(cardId){
+	cardsInPlay.push (cards[cardId])
+	console.log("user flipped card " + cards[cardId]);
+	chechForMatch();
+}
+flipCard(0);
+flipCard(1);
